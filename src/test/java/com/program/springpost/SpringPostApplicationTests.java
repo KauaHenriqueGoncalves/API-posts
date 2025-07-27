e76e5b1550dea89170cc2a95335bef141eef7d4a
KauaHenriqueGoncalves <Kaua.Goncalves0125@jala.university>
2025-07-27 00:01:07 -0300

    first commit

---
+++ src/test/java/com/program/springpost/SpringPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.springpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
